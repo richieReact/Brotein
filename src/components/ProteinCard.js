70323ed0a8e09d67d013d6d13e2537a1ed60d1bb
Richard Channell <richardchannelljr@gmail.com>
2021-05-17 23:48:21 -0400

    Finished component, room for sharpening styling

---
+++ src/components/ProteinCard.js
@@ -0,0 +1,94 @@
+import React, { useState } from 'react'
+import { View, Text, StyleSheet } from 'react-native'
+import { Avatar, Card, Title, Paragraph, withTheme, Button, TextInput } from 'react-native-paper';
+import { colors, Input } from 'react-native-elements'
+
+import Spacer from './Spacer'
+
+const ProteinCard = ({ theme }) => {
+  const [protein, setProtein] = useState(0)
+  const [newProtein, setNewProtein] = useState(0)
+  const [delProtein, setDelProtein] = useState(0)
+
+  const { colors } = theme
+
+  return (
+    <>
+      <View style={{ color: colors.primary }} >
+
+      <View>
+        <Card>
+          <Card.Title title="Today's Protein" />
+          <Card.Content>
+            <Title>{protein} grams</Title>
+            <Paragraph>
+              We recommend at least 1 gram of protein per pound of lean bodyweight!
+            </Paragraph>
+          </Card.Content>
+          {/* <Card.Cover source={{ uri: 'https://picsum.photos/700' }} /> */}
+          {/* <Card.Actions>
+            <Button>Cancel</Button>
+            <Button>Ok</Button>
+          </Card.Actions> */}
+        </Card>
+      </View>
+
+      <Spacer />
+
+      <View style={{ color: colors.primary }} >
+        <Button onPress={() => setProtein('0')} mode='outlined'>Reset Protein Count</Button>
+      </View>
+
+      <Spacer />
+
+      <View style={styles.input} >
+        <TextInput 
+          style={{ flex: 1 }}
+          label='Add protein'
+          value={newProtein}
+          onChangeText={(num) => setNewProtein(num)}
+        />
+        <Button 
+          contentStyle={{ height: 60, width: 60 }}
+          compact
+          icon='plus'
+          onPress={() => setProtein(Number(protein) + Number(newProtein))} 
+          mode='outlined' 
+          style={{ height: 60, justifyContent: 'center', alignContent: 'center' }}
+        ></Button>
+        
+        <TextInput 
+          style={{ flex: 1 }}
+          label='Subtract protein'
+          value={delProtein}
+          onChangeText={(num) => setDelProtein(num)}
+        />
+        <Button 
+          contentStyle={{ height: 60, width: 60 }}
+          compact
+          icon='minus'
+          onPress={() => setProtein(Number(protein) - Number(delProtein))} 
+          mode='outlined' 
+          style={{ height: 60, justifyContent: 'center', alignContent: 'center' }}
+        ></Button>
+      </View> 
+
+      {/* <TextInput /> */}
+
+      </View>
+    </>
+  )
+}
+
+const styles = StyleSheet.create({
+  input: {
+    width: '100%',
+    flexDirection: 'row',
+    height: 61
+  },
+  button: {
+    color: colors.primary
+  }
+})
+
+export default withTheme(ProteinCard)
