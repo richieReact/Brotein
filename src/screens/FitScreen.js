b1a4020c299168cf61a43407662f0b4158de35ea
Richard Channell <richardchannelljr@gmail.com>
2021-05-17 23:49:37 -0400

    Extracted some logic and started the app bar

--- src/screens/FitScreen.js
+++ src/screens/FitScreen.js
@@ -1,55 +1,53 @@
 import React, { useState, useEffect } from 'react'
 import { View, StyleSheet, Text } from 'react-native'
-import { Avatar, Card, Title, Paragraph } from 'react-native-paper';
-import { Button, Input } from 'react-native-elements'
+import { colors } from 'react-native-elements'
+import { Appbar, withTheme } from 'react-native-paper'
 
-const FitScreen = () => {
-  const [protein, setProtein] = useState(0)
-  const [newProtein, setNewProtein] = useState(0)
+import ProteinCard from '../components/ProteinCard'
+
+import BigSpacer from '../components/BigSpacer'
+
+const FitScreen = ({ navigation }) => {
 
   return (
     <>
-    <View>
-      <Card>
-        <Card.Title title="Today's Protein"  />
-        <Card.Content>
-          <Title>{protein} grams</Title>
-          {/* <Paragraph>Card content</Paragraph> */}
-        </Card.Content>
-        {/* <Card.Cover source={{ uri: 'https://picsum.photos/700' }} /> */}
-        {/* <Card.Actions>
-          <Button>Cancel</Button>
-          <Button>Ok</Button>
-        </Card.Actions> */}
-    </Card>
-    </View>
-    <Button 
-      title='Reset Protein count'
-      raised
-      onPress={() => setProtein('0')}
-    />
-    <View style={styles.input} >
-      <Input 
-        label='Add tein'
-        value={newProtein}
-        onChangeText={(num) => setNewProtein(num)}
-      />
-      <Button
-        title='Add more tein'
-        onPress={() => setProtein(Number(protein) + Number(newProtein))}
-      />
-    </View>
-    
-  </>
-
+      <ProteinCard />
+      <Appbar style={styles.bottom}>
+        <Appbar.Action
+          icon="account"
+          onPress={() => navigation.navigate('Account')}
+          />
+        <BigSpacer />
+        <Appbar.Action icon="mail" onPress={() => console.log('Pressed mail')} />
+        <BigSpacer />
+        <Appbar.Action icon="label" onPress={() => console.log('Pressed label')} />
+        <BigSpacer />
+        <Appbar.Action
+            icon="delete"
+            onPress={() => console.log('Pressed delete')}
+          />
+        </Appbar>
+    </>
   )
 }
 
+FitScreen.navigationOptions = () => {
+  return {
+    title: 'Brotein, your simple fitness tracker'
+  
+  }
+}
+
 const styles = StyleSheet.create({
-  input: {
-    width: 100,
-    flexDirection: 'row'
+  bottom: {
+    position: 'absolute',
+    left: 0,
+    right: 0,
+    bottom: 0,
+    color: colors.primary,
+    height: 65,
+    justifyContent: 'center'
   }
 })
 
-export default FitScreen
+export default withTheme(FitScreen)
