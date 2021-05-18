deaf2efa27ae57ae964d3ce6c058fba70e38cfcf
Richard Channell <richardchannelljr@gmail.com>
2021-05-17 23:48:55 -0400

    Just the bare minimum, gotta plan this out still

---
+++ src/screens/AccountScreen.js
@@ -0,0 +1,17 @@
+import React, { useState, useEffect } from 'react'
+import { View, StyleSheet, Text } from 'react-native'
+
+const AccountScreen = () => {
+
+  return (
+    <>
+     <Text>This will be the account screen</Text>
+    </>
+  )
+}
+
+const styles = StyleSheet.create({
+ 
+})
+
+export default AccountScreen
