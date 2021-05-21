3fde4680f895a3c0548b36d6bea163b5a86b1ab6
Richard Channell <richardchannelljr@gmail.com>
2021-05-21 09:48:20 -0400

    Added the water component

--- src/screens/FitScreen.js
+++ src/screens/FitScreen.js
@@ -4,29 +4,33 @@ import { colors } from 'react-native-elements'
 import { Appbar, withTheme } from 'react-native-paper'
 
 import ProteinCard from '../components/ProteinCard'
-
 import BigSpacer from '../components/BigSpacer'
+import Spacer from '../components/Spacer'
+import Water from '../components/Water'
 
 const FitScreen = ({ navigation }) => {
 
   return (
     <>
       <ProteinCard />
-      <Appbar style={styles.bottom}>
+      <Spacer />
+      <Water />
+      <Appbar style={styles.appBar}>
         <Appbar.Action
           icon="account"
           onPress={() => navigation.navigate('Account')}
-          />
-        <BigSpacer />
-        <Appbar.Action icon="mail" onPress={() => console.log('Pressed mail')} />
-        <BigSpacer />
-        <Appbar.Action icon="label" onPress={() => console.log('Pressed label')} />
+        />
         <BigSpacer />
         <Appbar.Action
-            icon="delete"
-            onPress={() => console.log('Pressed delete')}
-          />
-        </Appbar>
+          icon="weight-lifter" 
+          onPress={() => navigation.navigate('Lifties')} 
+        />
+        <BigSpacer />
+        <Appbar.Action
+          icon="delete"
+          onPress={() => console.log('Pressed delete')}
+        />
+      </Appbar>
     </>
   )
 }
@@ -39,13 +43,13 @@ FitScreen.navigationOptions = () => {
 }
 
 const styles = StyleSheet.create({
-  bottom: {
+  appBar: {
     position: 'absolute',
     left: 0,
     right: 0,
     bottom: 0,
     color: colors.primary,
-    height: 65,
+    height: 70,
     justifyContent: 'center'
   }
 })
