883408cb8dc2176b24ba6112bf33f5413f5c36a5
Richard Channell <richardchannelljr@gmail.com>
2021-05-21 09:48:41 -0400

    Initiated the LiftScreen

---
+++ src/screens/LiftScreen.js
@@ -0,0 +1,18 @@
+import React, { useEffect } from 'react'
+import { View, Text, StyleSheet } from 'react-native'
+import { withTheme } from 'react-native-paper'
+
+const LiftScreen = () => {
+
+  return (
+    <View>
+      <Text>Yoooo</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+
+})
+
+export default withTheme(LiftScreen)
