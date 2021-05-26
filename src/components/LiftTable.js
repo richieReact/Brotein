04717beb288ec6eb69e2c343bd78de4a3a696d73
Richard Channell <richardchannelljr@gmail.com>
2021-05-26 10:12:49 -0400

    Finished the modal menus and added the one rep max table. After 12 reps the max gets inflated, maybe I should limit the rep amount

--- src/components/LiftTable.js
+++ src/components/LiftTable.js
@@ -1,10 +1,10 @@
 import React, { useState } from 'react'
 import { View, Text, StyleSheet } from 'react-native'
-import { DataTable, withTheme, Title, Modal, Portal, Provider, Button, TextInput, Menu, Divider } from 'react-native-paper'
+import { DataTable, withTheme, Title, Modal, Portal, Provider, Button, TextInput, Menu, Paragraph } from 'react-native-paper'
 
 const LiftTable = () => {
   const [visible, setVisible] = useState(false);
-  const [text, setText] = useState('')
+  // const [text, setText] = useState('')
 
   // State for each lift and rep
   const [benchWeight, setBenchWeight] = useState('185')
@@ -53,34 +53,173 @@ const LiftTable = () => {
     }
   }
 
+  //Exercise controls for the modal
   const [squatControl, setSquatControl] = useState(false)
   const sWeightCnt = () => {
     if (squatControl) {
-      return <TextInput 
-      style={{ flex: 1 }}
-      label='Squat Weight'
-      value={squatWeight}
-      onChangeText={(num) => setSquatWeight(num)}
-    />
+      return (
+        <View>
+          <TextInput 
+            label='Bench Press Weight'
+            value={squatWeight}
+            onChangeText={(num) => setSquatWeight(num)}
+          />
+          <TextInput 
+            label='Bench Press Reps'
+            value={squatReps}
+            onChangeText={(num) => setSquatReps(num)}
+          />
+          <Button
+            style={{ width: 150, alignSelf: 'center' }}
+            mode='outlined'
+            onPress={() => setSquatControl(false)}  
+          >Finish Update</Button>
+        </View>
+      )
+    }
+  }
+
+  const [barPressControl, setBarPressControl] = useState(false)
+  const barPressWeightCnt = () => {
+    if (barPressControl) {
+      return (
+        <View>
+          <TextInput 
+            label='Bench Press Weight'
+            value={barPressWeight}
+            onChangeText={(num) => setBarPressWeight(num)}
+          />
+          <TextInput 
+            label='Bench Press Reps'
+            value={barPressReps}
+            onChangeText={(num) => setBarPressReps(num)}
+          />
+          <Button
+            style={{ width: 150, alignSelf: 'center' }}
+            mode='outlined'
+            onPress={() => setBarPressControl(false)}  
+          >Finish Update</Button>
+        </View>
+      )
+    }
+  }
+
+  const [barRowControl, setBarRowControl] = useState(false)
+  const barRowWeightCnt = () => {
+    if (barRowControl) {
+      return (
+        <View>
+          <TextInput 
+            label='Bench Press Weight'
+            value={barRowWeight}
+            onChangeText={(num) => setBarRowWeight(num)}
+          />
+          <TextInput 
+            label='Bench Press Reps'
+            value={barRowReps}
+            onChangeText={(num) => setBarRowReps(num)}
+          />
+          <Button
+            style={{ width: 150, alignSelf: 'center' }}
+            mode='outlined'
+            onPress={() => setBarRowControl(false)}  
+          >Finish Update</Button>
+        </View>
+      )
+    }
+  }
+
+  const [deadControl, setDeadControl] = useState(false)
+  const deadWeightCnt = () => {
+    if (deadControl) {
+      return (
+        <View>
+          <TextInput 
+            label='Bench Press Weight'
+            value={deadWeight}
+            onChangeText={(num) => setDeadWeight(num)}
+          />
+          <TextInput 
+            label='Bench Press Reps'
+            value={deadReps}
+            onChangeText={(num) => setDeadReps(num)}
+          />
+          <Button
+            style={{ width: 150, alignSelf: 'center' }}
+            mode='outlined'
+            onPress={() => setDeadControl(false)}  
+          >Finish Update</Button>
+        </View>
+      )
     }
   }
 
   // Makes the menu close after selecting the lift
-  const menuControl = () => {
+  const benchMenuControl = () => {
     setBenchControl(true)
     closeMenu()
   }
 
+  const squatMenuControl = () => {
+    setSquatControl(true)
+    closeMenu()
+  }
+
+  const barRowMenuControl = () => {
+    setBarRowControl(true)
+    closeMenu()
+  }
+
+  const barPressMenuControl = () => {
+    setBarPressControl(true)
+    closeMenu()
+  }
+
+  const deadMenuControl = () => {
+    setDeadControl(true)
+    closeMenu()
+  }
+
+  //Gonna try to make a one rep max calculator; ended up finding this one
+  const benchOneRepMax = () => {
+    const benchMax = Math.round((100 * benchWeight) / (101.3 - (2.67123 * benchReps)))
+    console.log(benchMax)
+    return benchMax
+  }
+
+  const squatOneRepMax = () => {
+    const squatMax = Math.round((100 * squatWeight) / (101.3 - (2.67123 * squatReps)))
+    console.log(squatMax)
+    return squatMax
+  }
+
+  const barRowOneRepMax = () => {
+    const barRowMax = Math.round((100 * barRowWeight) / (101.3 - (2.67123 * barRowReps)))
+    console.log(barRowMax)
+    return barRowMax
+  }
+
+  const barPressOneRepMax = () => {
+    const barPressMax = Math.round((100 * barPressWeight) / (101.3 - (2.67123 * barPressReps)))
+    console.log(barPressMax)
+    return barPressMax
+  }
+
+  const deadOneRepMax = () => {
+    const deadMax = Math.round((100 * deadWeight) / (101.3 - (2.67123 * deadReps)))
+    console.log(deadMax)
+    return deadMax
+  }
 
   return (
     <>
       <Provider>
         <Portal>
-          <Title style={{ textAlign: 'center' }} >Title</Title>
+          <Title style={{ textAlign: 'center', marginTop: 15 }}>The Staple Compound Lifts</Title>
           <DataTable>
             <DataTable.Header>
               <DataTable.Title>Exercises</DataTable.Title>
-              <DataTable.Title numeric>Weight</DataTable.Title>
+              <DataTable.Title numeric>Weight (lbs)</DataTable.Title>
               <DataTable.Title numeric>Reps</DataTable.Title>
             </DataTable.Header>
             <DataTable.Row>
@@ -113,6 +252,35 @@ const LiftTable = () => {
             Edit Lifts
           </Button>
 
+          <Title style={{ textAlign: 'center' }} >1 Rep Max</Title>
+          <DataTable>
+            <DataTable.Header>
+              <DataTable.Title>Exercises</DataTable.Title>
+              <DataTable.Title numeric>1 Rep Max</DataTable.Title>
+            </DataTable.Header>
+            <DataTable.Row>
+              <DataTable.Cell onPress={showModal}>Bench Press</DataTable.Cell>
+              <DataTable.Cell numeric>{benchOneRepMax()}</DataTable.Cell>
+            </DataTable.Row>
+            <DataTable.Row>
+              <DataTable.Cell>Squat</DataTable.Cell>
+              <DataTable.Cell numeric>{squatOneRepMax()}</DataTable.Cell>
+            </DataTable.Row>
+            <DataTable.Row>
+              <DataTable.Cell>Barbell Row</DataTable.Cell>
+              <DataTable.Cell numeric>{barRowOneRepMax()}</DataTable.Cell>
+            </DataTable.Row>
+            <DataTable.Row>
+              <DataTable.Cell>Barbell Press</DataTable.Cell>
+              <DataTable.Cell numeric>{barPressOneRepMax()}</DataTable.Cell>
+            </DataTable.Row>
+            <DataTable.Row>
+              <DataTable.Cell>Deadlift</DataTable.Cell>
+              <DataTable.Cell numeric>{deadOneRepMax()}</DataTable.Cell>
+            </DataTable.Row>
+          </DataTable>
+
+          {/* gotta keep this at the bottom */}
           <Modal visible={visible} onDismiss={hideModal} contentContainerStyle={containerStyle}>
             <View>
               <View style={styles.menu} >
@@ -120,34 +288,23 @@ const LiftTable = () => {
                   style={{ marginTop: 2 }}
                   visible={menuVisible}
                   onDismiss={closeMenu}
-                  anchor={<Button onPress={openMenu}>Which lift?</Button>}>
-                  <Menu.Item onPress={() => {menuControl()}} title="Bench Press" />
-                  <Menu.Item onPress={() => {setSquatControl(true)}} title="Squat" />
-                  <Menu.Item onPress={() => {}} title="Barbell Row" />
-                  <Menu.Item onPress={() => {}} title="Barbell Press" />
-                  <Menu.Item onPress={() => {}} title="Deadlift" />
+                  anchor={
+                    <Button mode='outlined' onPress={openMenu}>Which lift?</Button>}>
+                  <Menu.Item onPress={() => {benchMenuControl()}} title="Bench Press" />
+                  <Menu.Item onPress={() => {squatMenuControl()}} title="Squat" />
+                  <Menu.Item onPress={() => {barRowMenuControl()}} title="Barbell Row" />
+                  <Menu.Item onPress={() => {barPressMenuControl()}} title="Barbell Press" />
+                  <Menu.Item onPress={() => {deadMenuControl()}} title="Deadlift" />
                 </Menu>
               </View>
               {bWeightCnt()}
               {sWeightCnt()}
-              {/* <TextInput 
-                style={{ flex: 1 }}
-                label='Bench Press Weight'
-                value={benchWeight}
-                onChangeText={(num) => setBenchWeight(num)}
-              /> */}
-              {/* <Button 
-                contentStyle={{ height: 60, width: 60 }}
-                compact
-                icon='minus'
-                onPress={() => setProtein(Number(protein) - Number(delProtein))} 
-                mode='outlined' 
-                style={{ height: 60, justifyContent: 'center', alignContent: 'center' }}
-              ></Button> */}
+              {barRowWeightCnt()}
+              {barPressWeightCnt()}
+              {deadWeightCnt()}
             </View>
           </Modal>
-          
-          
+            
         </Portal>
       </Provider>
     </>
@@ -159,9 +316,10 @@ const styles = StyleSheet.create({
   
   },
   menu: {
-    position: 'absolute',
-    bottom: 260,
-    right: 120
+    // position: 'absolute',
+    // bottom: 260,
+    // right: 120,
+    alignSelf: 'center'
   }
 })
 
