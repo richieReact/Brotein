64aee7ef424df7211e963f3b62d61606633c704f
Richard Channell <richardchannelljr@gmail.com>
2021-05-27 00:37:02 -0400

    Added an AppBar

--- src/screens/LiftScreen.js
+++ src/screens/LiftScreen.js
@@ -1,21 +1,46 @@
 import React, { useEffect, useState } from 'react'
 import { View, Text, StyleSheet } from 'react-native'
-import { withTheme } from 'react-native-paper'
+import { withTheme, Appbar } from 'react-native-paper'
+import { colors } from 'react-native-elements'
 
 import LiftTable from '../components/LiftTable'
+import BigSpacer from '../components/BigSpacer'
 
-const LiftScreen = () => {
+const LiftScreen = ({ navigation }) => {
   
-
   return (
     <>
       <LiftTable />
+      <Appbar style={styles.appBar}>
+        <Appbar.Action
+          icon="account"
+          onPress={() => navigation.navigate('Account')}
+        />
+        <BigSpacer />
+        <Appbar.Action
+          icon="weight-lifter" 
+          onPress={() => navigation.navigate('Lifties')} 
+        />
+        <BigSpacer />
+        <Appbar.Action
+          icon="delete"
+          onPress={() => console.log('Pressed delete')}
+        />
+      </Appbar>
     </>
   )
 }
 
 const styles = StyleSheet.create({
-
+  appBar: {
+    position: 'absolute',
+    left: 0,
+    right: 0,
+    bottom: 0,
+    color: colors.primary,
+    height: 88,
+    justifyContent: 'center'
+  }
 })
 
 export default withTheme(LiftScreen)
