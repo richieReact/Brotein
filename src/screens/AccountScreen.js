393a67f68c87d6a26e67566b12f86e78623a69c5
Richard Channell <richardchannelljr@gmail.com>
2021-05-27 17:26:52 -0400

    Added an AppBar

--- src/screens/AccountScreen.js
+++ src/screens/AccountScreen.js
@@ -1,17 +1,48 @@
 import React, { useState, useEffect } from 'react'
 import { View, StyleSheet, Text } from 'react-native'
+import { Appbar } from 'react-native-paper'
+import { colors } from 'react-native-elements'
 
-const AccountScreen = () => {
+import BigSpacer from '../components/BigSpacer'
+
+const AccountScreen = ({ navigation }) => {
 
   return (
     <>
      <Text>This will be the account screen</Text>
+     <Appbar style={styles.appBar}>
+        <Appbar.Action
+          size={30}
+          icon="food"
+          onPress={() => navigation.navigate('Fit')}
+        />
+        <BigSpacer />
+        <Appbar.Action
+          size={30}
+          icon="weight-lifter" 
+          onPress={() => navigation.navigate('Lifties')} 
+        />
+        <BigSpacer />
+        <Appbar.Action
+          size={30}
+          icon="account"
+          onPress={() => navigation.navigate('Account')}
+        />
+      </Appbar>
     </>
   )
 }
 
 const styles = StyleSheet.create({
- 
+  appBar: {
+    position: 'absolute',
+    left: 0,
+    right: 0,
+    bottom: 0,
+    color: colors.primary,
+    height: 88,
+    justifyContent: 'center'
+  }
 })
 
 export default AccountScreen
