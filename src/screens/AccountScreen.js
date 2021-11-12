f6612bfa978d3cd9db41dcb860d15672384917a2
Richard Channell <richardchannelljr@gmail.com>
2021-11-11 22:57:28 -0500

    Alright, got a good start on the calendar! Still gotta set up how to reverse the marking and figure out how to track the workout type, but this is a triumph!

--- src/screens/AccountScreen.js
+++ src/screens/AccountScreen.js
@@ -4,15 +4,44 @@ import { Appbar } from 'react-native-paper'
 import { colors } from 'react-native-elements'
 
 import { Calendar, CalendarList, Agenda } from 'react-native-calendars'
+import AsyncStorage from '@react-native-async-storage/async-storage'
 
 import BigSpacer from '../components/BigSpacer'
 
 const AccountScreen = ({ navigation }) => {
+  const [markedDates, setMarkedDates] = useState([{}])
+
+  const STORAGE_KEY = '@save_dates'
+
+  useEffect(() => {}, [])
+
+  // idea for how to save the dates in the function below:
+  // 1. Load in saved markedDates from AsyncStorage to gather the previously markedDates...
+  // ... should do this on page load (useEffect), make it an array right?
+  // 2. Push a new object onto the markedDates array with those props it needs
+  // 3. Set the state and save it in AsyncStorage
+
+  const dayEvents = (date) => {
+    let mark = {}
+    mark[date] = {
+      selected: true,
+      color: '#4B0082',
+    }
+
+    // const dateObject = {
+    //   selected: true,
+    //   color: '#4B0082',
+    // }
+
+    setMarkedDates({ ...markedDates, ...mark }) // holy shit lmfao it works
+    console.log(markedDates)
+  }
+
   return (
     <>
       <Text style={{ alignSelf: 'center', margin: 15, fontSize: 25 }}>Track your Workouts</Text>
       <View>
-        <Calendar />
+        <Calendar onDayPress={(day) => dayEvents(day.dateString)} markedDates={markedDates} />
       </View>
       <Appbar style={styles.appBar}>
         <Appbar.Action size={30} icon='food' onPress={() => navigation.navigate('Fit')} />
