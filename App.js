d6a7f80b2d5d260d6fc1054c76e1a01b65bc2de3
Richard Channell <richardchannelljr@gmail.com>
2022-04-06 20:35:22 -0400

    Let's give it a go!

--- App.js
+++ App.js
@@ -1,8 +1,6 @@
 import React from 'react'
-import { createAppContainer, createSwitchNavigator } from 'react-navigation'
+import { createAppContainer } from 'react-navigation'
 import { createStackNavigator } from 'react-navigation-stack'
-import { createBottomTabNavigator } from 'react-navigation-tabs'
-import { StyleSheet, Text, View } from 'react-native'
 
 import { DefaultTheme, Provider as PaperProvider, DarkTheme } from 'react-native-paper'
 
